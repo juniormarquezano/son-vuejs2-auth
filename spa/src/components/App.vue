a3c4f9aafe6923ec89f512cafa94bd98cf21e8c7
Junior Marquezano <juniormarquezano@gmail.com>
2017-09-01 09:07:50 -0300

    Criando navegação com vue-router

--- spa/src/components/App.vue
+++ spa/src/components/App.vue
@@ -2,51 +2,17 @@
     <div class="container">
         <div class="row">
             <h1>BRASILEIRÃO SÉRIE A</h1>
-            <!--<a href="#" class="btn btn-primary" @click.prevent="showNovoJogo">Novo Jogo</a><br /><br />-->
-            <!-- DIV novoJogo -->
-            <a href="#" class="btn btn-primary" @click.prevent="showNovoJogo">Novo Jogo</a>
-            <a href="#" class="btn btn-primary" @click.prevent="showTabela">Ver Tabela</a>
+            <a href="#" class="btn btn-primary">Novo Jogo</a>
+            <a href="#" class="btn btn-primary">Ver Tabela</a>
             <hr>
-            <div v-if="view == 'novojogo'">
-                <time-jogo></time-jogo>
-            </div>
-            <!-- /DIV novoJogo -->
-            <!-- DIV tabela -->
-            <div v-show="view == 'tabela'">
-                <time-list></time-list>
-                <time-zona></time-zona>
-            </div>
-            <!-- /DIV tabela -->
+            <router-view></router-view><!-- declaração da rota -->
         </div>
     </div>
 </template>
 
 <script type="text/javascript">
-    import TimeListComponent from './TimeList.vue';
-    import TimeJogoComponent from './TimeJogo.vue';
-    import TimeZonaComponent from './TimeZona.vue';
-    import { store } from '../store'
-
     export default {
-        components: {
-            'time-list': TimeListComponent,
-            'time-jogo': TimeJogoComponent,
-            'time-zona': TimeZonaComponent,
-        },
-        computed: {
-            view(){
-                return store.state.view;
-                //return this.$store.state.view;
-            }
-        },
-        methods: {
-            showNovoJogo() {
-                store.commit('show-time-novojogo');
-            },
-            showTabela() {
-                store.commit('show-time-list');
-            }
-        }
+        //
     }
 </script>
 
